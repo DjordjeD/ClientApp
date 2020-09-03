307fd141be16d9dd8f2d8781a40a05d9f6b1d3de
DjordjeD <serbiasistem@gmail.com>
2020-09-03 04:17:30 +0200

    Add project files.

---
+++ ClientApp/LoginScreen.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ClientApp
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.LogInButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label323 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label323);
+            this.panel1.Controls.Add(this.NameTextBox);
+            this.panel1.Location = new System.Drawing.Point(2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(718, 242);
+            this.panel1.TabIndex = 0;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.NameTextBox.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTextBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.NameTextBox.Location = new System.Drawing.Point(136, 186);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(357, 36);
+            this.NameTextBox.TabIndex = 0;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.SurnameTextBox.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SurnameTextBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.SurnameTextBox.Location = new System.Drawing.Point(138, 296);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(357, 36);
+            this.SurnameTextBox.TabIndex = 1;
+            // 
+            // LogInButton
+            // 
+            this.LogInButton.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogInButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.LogInButton.Location = new System.Drawing.Point(271, 373);
+            this.LogInButton.Name = "LogInButton";
+            this.LogInButton.Size = new System.Drawing.Size(109, 45);
+            this.LogInButton.TabIndex = 2;
+            this.LogInButton.Text = "LogIn";
+            this.LogInButton.UseVisualStyleBackColor = true;
+            this.LogInButton.Click += new System.EventHandler(this.LogInButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(119)))), ((int)(((byte)(121)))));
+            this.pictureBox1.BackgroundImage = global::ClientApp.Properties.Resources.algotech_RGB_300dpi;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 354);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label323
+            // 
+            this.label323.AutoSize = true;
+            this.label323.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label323.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.label323.Location = new System.Drawing.Point(131, 142);
+            this.label323.Name = "label323";
+            this.label323.Size = new System.Drawing.Size(72, 29);
+            this.label323.TabIndex = 1;
+            this.label323.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.label1.Location = new System.Drawing.Point(133, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Surname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(170)))), ((int)(((byte)(213)))));
+            this.label2.Location = new System.Drawing.Point(43, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(558, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Algotech Login Screen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(190, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Welcome to Algotech";
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(647, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LogInButton);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginScreen";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.Button LogInButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label323;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ClientApp/LoginScreen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+
+
+namespace ClientApp
+{
+    public partial class LoginScreen : Form
+    {
+        localhost.WebService1 webservice = new localhost.WebService1();
+        HttpClient client = new HttpClient();
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void LogInButton_Click(object sender, EventArgs e)
+        {
+            string name = NameTextBox.Text.ToString();
+            string surname = SurnameTextBox.Text.ToString();
+            //name = name + surname;
+            client.BaseAddress = new Uri("http://localhost:60836/WebService1.asmx/");
+            HttpResponseMessage result = client.GetAsync("Find?name=" + name +"&surname="+surname+"").Result;
+
+            string information = result.Content.ReadAsStringAsync().Result;
+           // MessageBox.Show(information);
+
+            if (information.Contains("true")) MessageBox.Show("Welcome to Algotech, you are a VIP user!");
+            else MessageBox.Show("Welcome to Algotech, you are a regular user.");
+
+
+
+        }
+    }
+}
